06d4f017d6feeb0e7ef38bcd07d9fcb70d9fe626
Daniel <daniel@danramteke.com>
2020-12-11 04:03:40 -0500

    new action

--- Sources/LeafPressKit/actions/NewAction/NewAction.swift
+++ Sources/LeafPressKit/actions/NewAction/NewAction.swift
@@ -12,7 +12,27 @@ public class NewAction {
   public func scaffold(dryRun: Bool, date: Date) -> Result<String, Error> {
 
     let path = config.distDir + config.postsDir
+    let dateString = config.string(from: date)
+    let content = self.newContent(dateString: dateString)
+
+    if !dryRun {
+      do {
+        try path.write(content.data(using: .utf8)!)
+      } catch {
+        return .failure(error)
+      }
+    }
     return .success(path.string)
   }
-}
 
+  func newContent(dateString: String) -> String {
+    """
+    title: New Post on \(dateString)
+    date: \(dateString)
+    ---
+    # New Post on \(dateString)
+
+    Today I wrote a blog post.
+    """
+  }
+}
