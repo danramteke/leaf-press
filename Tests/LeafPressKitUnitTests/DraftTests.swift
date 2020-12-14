3222c23254784738aad671a74584a58636f7293f
Daniel <daniel@danramteke.com>
2020-12-14 01:45:01 -0500

    tests for drafts

---
+++ Tests/LeafPressKitUnitTests/DraftTests.swift
@@ -0,0 +1,57 @@
+import Foundation
+import XCTest
+@testable import LeafPressKit
+
+
+class DraftTests: XCTestCase {
+  func testNotDraft() throws {
+    let string = """
+    title: Hello
+    draft: false
+    ----
+    # Hello world
+
+    How are you?
+    """
+    XCTAssertTrue(try self.markdownInputFile(string: string).isIncluded)
+  }
+
+  func testDraft() throws {
+    let string = """
+    title: Hello
+    draft: true
+    ----
+    # Hello world
+
+    How are you?
+    """
+    XCTAssertFalse(try self.markdownInputFile(string: string).isIncluded)
+  }
+
+  func testUnparsebleDraftIsStillConsideredDraft() throws {
+    let string = """
+    title: Hello
+    draft: unparsable
+    ----
+    # Hello world
+
+    How are you?
+    """
+    XCTAssertFalse(try self.markdownInputFile(string: string).isIncluded)
+  }
+
+  func testMissingDraftIsNotConsideredDraft() throws {
+    let string = """
+    title: Hello
+    ----
+    # Hello world
+
+    How are you?
+    """
+    XCTAssertTrue(try self.markdownInputFile(string: string).isIncluded)
+  }
+
+  private func markdownInputFile(string: String) throws -> InputFile {
+    return try InputFile(string: string, at: FileLocation(root: "", directoryPath: "", rawFilename: "rawFilename.md", slug: "rawFilename", fileExtension: "md"))
+  }
+}
