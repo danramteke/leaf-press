6575af2c4568b32f166edb169cc25c0c620396c7
Daniel <daniel@danramteke.com>
2020-12-14 20:33:43 -0500

    rename

--- Sources/LeafPressKit/Models/Post.init.swift
+++ Sources/LeafPressKit/Models/Post.init.swift
@@ -13,11 +13,11 @@ extension Post: InputFileInitable {
       fileExtension: SupportedFileType.html.rawValue)
 
     guard let dateString = inputFile.dateString else {
-      throw PageInitError(path: inputFile.source.relativeURL.relativeString, message: "doesn't have date. Add a `date` field to the front matter of the post. Frontmatter is YAML at the start of file. A '---' marks the beginning of the YAML and a second '---' marks the end of the YAML.")
+      throw PostInitError(path: inputFile.source.relativeURL.relativeString, message: "doesn't have date. Add a `date` field to the front matter of the post. Frontmatter is YAML at the start of file. A '---' marks the beginning of the YAML and a second '---' marks the end of the YAML.")
     }
 
     guard let date = config.date(from: dateString.rawValue) else {
-      throw PageInitError(path: inputFile.source.relativeURL.relativeString, message: "couldn't parse date from \(dateString.rawValue)")
+      throw PostInitError(path: inputFile.source.relativeURL.relativeString, message: "couldn't parse date from \(dateString.rawValue)")
     }
     
     self.init(
@@ -34,7 +34,7 @@ extension Post: InputFileInitable {
       isIncluded: inputFile.isIncluded)
   }
 
-  struct PageInitError: Error, LocalizedError {
+  struct PostInitError: Error, LocalizedError {
     let path: String
     let message: String
 
